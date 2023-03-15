e90aa8858e5982c466ea0c7fbcf5b525a66acc1b
ils-juhis <j.sahu@thesynapses.com>
2023-03-15 16:06:26 +0530

    validation completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,35 @@ let toggleEye = (e)=>{
 }
 
 
+let validateName= (event)=>{
+    let name;
+    if(event.currentTarget===undefined)
+        name=event.value;
+    else
+        name=event.currentTarget.value;
+
+    let messageEmail = document.getElementById("invalid-name")
+
+    if(name===""){
+        messageEmail.innerHTML= "*Please enter valid name.";
+        return false;
+    }else{
+        messageEmail.innerHTML= `<span>&nbsp;</span>`
+        return true;
+    }
+}
+
 let validateEmail= (event)=>{
+    let email;
+    if(event.currentTarget===undefined)
+        email=event.value;
+    else
+        email=event.currentTarget.value;
+
     let messageEmail = document.getElementById("invalid-email")
-    console.log(event.currentTarget.value)
 
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let result = regex.test(event.currentTarget.value);
+    let result = regex.test(email);
     if(!result){
         messageEmail.innerHTML= "*Please enter valid email address";
         return false;
@@ -28,21 +51,30 @@ let validateEmail= (event)=>{
 }
 
 let validatePassword =(event)=>{
+    let password;
+    if(event.currentTarget===undefined)
+        password=event.value;
+    else
+        password=event.currentTarget.value;
+
     let messagePass = document.getElementById("invalid-password")
 
-    if(! /[a-z]/.test(event.currentTarget.value)){
+    if(password === ""){
+        messagePass.innerHTML= "*Please enter valid password.";
+        return false;
+    }else if(! /[a-z]/.test(password)){
         messagePass.innerHTML= "*Password should contain altleast 1 lower character.";
         return false;
-    }if(! /[A-Z]/.test(event.currentTarget.value)){
+    }else if(! /[A-Z]/.test(password)){
         messagePass.innerHTML= "*Password should contain altleast 1 upper character.";
         return false;
-    }if(! /[0-9]/.test(event.currentTarget.value)){
+    }else if(! /[0-9]/.test(password)){
         messagePass.innerHTML= "*Password should contain altleast 1 digit.";
         return false;
-    }if(! /[!@#$%^&*]/.test(event.currentTarget.value)){
+    }else if(! /[!@#$%^&*]/.test(password)){
         messagePass.innerHTML= "*Password should contain altleast 1 special character.";
         return false;
-    }if(event.currentTarget.value.length<8){
+    }else if(password.length<8){
         messagePass.innerHTML= "*Password should be minimum of 8 charaters.";
         return false;
     }else{
@@ -52,8 +84,13 @@ let validatePassword =(event)=>{
 }
 
 let comparePassword = (event)=>{
+    let cpassword;
+    if(event.currentTarget===undefined)
+        cpassword=event.value;
+    else
+        cpassword=event.currentTarget.value;
+
     let password = document.getElementById("input-password").value
-    let cpassword = event.currentTarget.value;
     let messagePass = document.getElementById("invalid-cpassword")
     if(password !== cpassword){
         messagePass.innerHTML= "*Password doesn't match.";
@@ -64,6 +101,84 @@ let comparePassword = (event)=>{
     }
 }
 
+//validate login and forgot password form
+let validateForm=(event, formName, lastStep)=>{
+    event.preventDefault();
+    let formFields = {
+        "login-form": ["email", "password"],
+        "forgot-email-form": ["email"],
+    }
+
+    fields=formFields[formName];
+    let fieldname, result=true, r;
+    for (let i = 0; i < fields.length; i++) {
+        fieldname = fields[i];
+        switch(fieldname){
+            case "email": result = result && validateEmail(document.forms[formName]["email"]);
+            break;
+            case "password": r = validatePassword(document.forms[formName]["password"]); result = result && r;
+            break;
+        }
+    }
+    if((formName === "login-form") && result){
+        document.forms[formName].submit();
+        window.location.href='dashboard.html';
+    }else if(formName === "forgot-email-form" && result){
+        document.forms[formName].submit();
+    }
+    return result;
+}
+//validate login and forgot password form
+let validateRegisterForm=(event, currentTab, lastStep)=>{
+    event.preventDefault();
+    let formFields = [["email", "password", "confirmPassword", "fname", "lname", "phoneNo"], 
+    ["schoolName", "state", "country", "zipCode"]]
+
+    fields=formFields[currentTab];
+    let fieldname, result=true, r;
+    for (let i = 0; i < fields.length; i++) {
+        fieldname = fields[i];
+        switch(fieldname){
+            case "email": result = result && validateEmail(document.forms["register-form"]["email"]);
+            break;
+            case "password": r = validatePassword(document.forms["register-form"]["password"]); result = result && r;
+            break;
+            case "confirmPassword": r = comparePassword(document.forms["register-form"]["confirmPassword"]); result = result && r;
+            break;
+            case "fname" ||"lname": r = validateName(document.forms["register-form"]["fname"]); result = result && r;
+                r = validateName(document.forms["register-form"]["lname"]); result = result && r;
+            break;
+            case "phoneNo": if(document.forms["register-form"]["phoneNo"].value === "")
+                            {
+                                document.getElementById("invalid-phone").innerHTML = "*Please enter valid mobile number."
+                                r=false;
+                            }else{
+                                document.getElementById("invalid-phone").innerHTML = `<span>&nbsp;</span>`
+                                r=true;
+                            }
+                            result = result && r;
+                            break;
+            default: if(document.forms["register-form"][fieldname].value === "")
+                    {
+                        document.getElementById("invalid-fields").innerHTML = "*Please fill all fields."
+                        r=false;
+                    }else{
+                        document.getElementById("invalid-fields").innerHTML = `<span>&nbsp;</span>`
+                        r=true;
+                    }
+                    result = result && r;
+        }
+    }
+
+    if((lastStep) && result && document.getElementById("condition-check").checked){
+        document.forms["register-form"].submit();
+        window.location.href='dashboard.html';
+    }
+    return result;
+}
+
+
+
 var currentTab = 0; 
 let showTab = (n) => {
   let x = document.getElementsByClassName("tab");
@@ -72,9 +187,9 @@ let showTab = (n) => {
   fixStepIndicator(n)
 }
 
-let nextPrev = (n) => {
+let nextPrev = (e, n) => {
     let x = document.getElementsByClassName("tab");
-    if (n == 1 && !(true)) 
+    if (n>=0 && !validateRegisterForm(e, currentTab, x.length-1===currentTab)) 
         return false;
     x[currentTab].style.display = "none";
     currentTab = currentTab + n;
@@ -120,34 +235,3 @@ const phoneInput = window.intlTelInput(phoneInputField, {
     utilsScript:
       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
    });
-
-
-// let fetchCode = async()=>{
-//     let response =  await fetch("https://restcountries.com/v3.1/all")
-//     data=await response.json();
-//     console.log(data[0]);
-
-//     function compare( a, b ) {
-//         if ( a.name.common < b.name.common ){
-//           return -1;
-//         }
-//         if ( a.name.common > b.name.common ){
-//           return 1;
-//         }
-//         return 0;
-//       }
-      
-//       data.sort( compare );
-
-//     let select = document.getElementById("select-country");
-
-//     for(let i=0; i<data.length; i++){
-//         select.innerHTML += `<option value="${data[i].idd.root+ data[i].idd.suffixes[0]}">
-//             <span  style="width: 20px;background-image:url(${data[i].flags.svg})"></span>
-//             ${data[i].name.common} (${data[i].idd.root+ data[i].idd.suffixes[0]})
-//         </option>`;
-//     }
-
-// }
-
-// fetchCode()
